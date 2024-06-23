2d3706591c1dcf373a153c19bf50ad5b376e5484
Soila Pertet <nicolepertet@gmail.com>
2024-06-23 03:53:18 -0600

    Initialise Task class

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
+// Define Task class
+class Task {
+
+  // initialise the constructor method of the Task object
+  constructor(
+    taskSupervisor,
+    clubMember, 
+    taskPriority,
+    taskDeadline,
+    assignedTask,
+    taskStatus
+  ) 
+  
+  // initialise the instance variables of the Task object
+  {
+    this.taskSupervisor = taskSupervisor;
+    this.clubMember = clubMember;
+    this.taskPriority = taskPriority;
+    this.taskDeadline = taskDeadline;
+    this.assignedTask = assignedTask;
+  }
+
+  // initialise the setter methods
+  // method to set the status of the current task
+  setTaskStatus(status) {
+    this.taskStatus = status;
+  }
+}
+
 // Initialisation
 let taskList = [];  // initialise an empty array
 let popupForm = document.querySelector("#popup-form");
 let overlay = document.querySelector(".overlay");
 
+
 // Define functions
 // Display popup form when plus button is clicked
 const displayPopupForm = () => {
   popupForm.classList.add("show");
+  popupForm.classList.remove("hide");
   overlay.classList.add("show");
 }
 
