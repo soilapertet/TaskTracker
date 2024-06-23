ae492aaa77033e18fb4991ba81b8de0876c6e3fc
Soila Pertet <nicolepertet@gmail.com>
2024-06-23 00:04:00 -0600

    Add functionality to close popup form

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 // Initialisation
 let taskList = [];  // initialise an empty array
+let popupForm = document.querySelector("#popup-form");
+let overlay = document.querySelector(".overlay");
 
 // Define functions
-
-// Display popup form when plus button is clocked
+// Display popup form when plus button is clicked
 const displayPopupForm = () => {
-  let popupForm = document.querySelector("#popup-form");
-  let overlay = document.querySelector(".overlay");
   popupForm.classList.add("show");
   overlay.classList.add("show");
+}
+
+// Close popup form when close button is clicked
+const closePopupForm = () => {
+  popupForm.classList.remove("show");
+  popupForm.classList.add("hide");
+  overlay.classList.remove("show");
 }
 
 // Functionality to open pop-up form
 let plusBtn = document.querySelector(".button_plus");
 plusBtn.addEventListener("click", displayPopupForm);
+
+// Functionality to close pop-up form
+let closeBtn = document.querySelector(".btn-close");
+closeBtn.addEventListener("click", closePopupForm);
